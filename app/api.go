978b121377f4365ac33bee649ac6dfc8b2395148
vikrammel <vmelkote@ucsc.edu>
2018-03-24 14:13:25 -0700

    moved api functions into different file

---
+++ app/api.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"encoding/json"
+	// "log"
+	"net/http"
+	// "os"
+	// "strings"
+	// "strconv"
+
+	// "ipsorting"
+
+	"github.com/gorilla/mux" //import router
+)
+
+// GetAllKeys displays all from the keyVals var
+func GetAllKeys(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(keyVals) //200
+}
+
+// GetKey returns a single key-value pair
+func GetKey(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	if params["key"] == "get_partition_id" {
+		json.NewEncoder(w).Encode(partitionID) //200
+		return
+	}
+
+	for key, value := range keyVals {
+		if key == params["key"] {
+			json.NewEncoder(w).Encode(value) //200
+			return
+		}
+	}
+
+	w.WriteHeader(http.StatusNotFound) //404
+	json.NewEncoder(w).Encode(&map[string]string{"Error": "Key not found"})
+}
+
+// PutKey creates/updates a key
+func PutKey(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	value := ""
+
+	//error checking inputs
+	if r.Body == nil {
+		//http.Error(w, "Please send a request body", 400)
+		w.WriteHeader(http.StatusBadRequest) //400
+		json.NewEncoder(w).Encode(&map[string]string{"Error": "No request body"})
+		return
+	}
+	value = r.PostFormValue("val")
+	if !(len(value) > 0) {
+		value = ""
+	} //make sure value is a valid empty str
+	keyVals[params["key"]] = value //store/update user's value for key
+
+	json.NewEncoder(w).Encode(&map[string]string{"Success": "key value pair {'" + params["key"] + "':'" + value + "'} updated"}) //200
+}
+
+// DeleteKey deletes a key
+func DeleteKey(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for key, value := range keyVals {
+		if key == params["key"] {
+			delete(keyVals, params["key"])
+			json.NewEncoder(w).Encode(&map[string]string{"Success": "key value pair {'" + params["key"] + "':'" + value + "'} deleted"}) //200
+			return
+		}
+	}
+
+	w.WriteHeader(http.StatusNotFound) //404
+	json.NewEncoder(w).Encode(&map[string]string{"Error": "Key not found"})
+}
+
+// DeleteAll deletes all key:value pairs in the kvs map
+func DeleteAll(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	for key := range keyVals {
+		delete(keyVals, key)
+	}
+
+	json.NewEncoder(w).Encode(&map[string]string{"Success": "Key-Value Store cleared"}) //200
+}
--- app/main.go
+++ app/main.go
@@ -1,16 +1,17 @@
 //main file for running node in kvs
-//written by Vikram and Mike
+//written by Vikram
 //note: remember, when file is saved in vs code, code is auto formatted and unused
 // code is removed. Make sure to comment out unused code or it won't persist
 
 package main
 
 import (
-	"encoding/json"
+	// "encoding/json"
 	"log"
 	"net/http"
 	"os"
 	"strings"
+	"strconv"
 
 	"ipsorting"
 
@@ -21,87 +22,25 @@ import (
 var keyVals map[string]string //map (dictionary) of string:string
 var ipport string             //node's own "<IP:Port>"
 var view []string             //node's initial view passed in through env
-var nodesPerCluster int		  //number of nodes in each cluster
-
-// GetAllKeys displays all from the keyVals var
-func GetAllKeys(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(keyVals) //200
-}
-
-// GetKey displays a single data
-func GetKey(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	for key, value := range keyVals {
-		if key == params["key"] {
-			json.NewEncoder(w).Encode(value) //200
-			return
-		}
-	}
-
-	w.WriteHeader(http.StatusNotFound) //404
-	json.NewEncoder(w).Encode(&map[string]string{"Error": "Key not found"})
-}
-
-// PutKey creates/updates a key
-func PutKey(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	value := ""
-
-	//erro checking inputs
-	if r.Body == nil {
-		//http.Error(w, "Please send a request body", 400)
-		w.WriteHeader(http.StatusBadRequest) //400
-		json.NewEncoder(w).Encode(&map[string]string{"Error": "No request body"})
-		return
-	}
-	value = r.PostFormValue("val")
-	if !(len(value) > 0) {
-		value = ""
-	} //make sure value is a valid empty str
-	keyVals[params["key"]] = value //store/update user's value for key
-
-	json.NewEncoder(w).Encode(&map[string]string{"Success": "key value pair {'" + params["key"] + "':'" + value + "'} updated"}) //200
-}
-
-// DeleteKey deletes a key
-func DeleteKey(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-
-	for key, value := range keyVals {
-		if key == params["key"] {
-			delete(keyVals, params["key"])
-			json.NewEncoder(w).Encode(&map[string]string{"Success": "key value pair {'" + params["key"] + "':'" + value + "'} deleted"}) //200
-			return
-		}
-	}
-
-	w.WriteHeader(http.StatusNotFound) //404
-	json.NewEncoder(w).Encode(&map[string]string{"Error": "Key not found"})
-}
-
-// DeleteAll deletes all key:value pairs in the kvs map
-func DeleteAll(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	for key := range keyVals {
-		delete(keyVals, key)
-	}
-
-	json.NewEncoder(w).Encode(&map[string]string{"Success": "Key-Value Store cleared"}) //200
-}
+var nodesPerCluster int		  //number of nodes in each cluster ('K' from env)
+var partitionID int			  //partition index node belongs to
 
 //init
 func main() {
 	router := mux.NewRouter()                    //init router
 	keyVals = make(map[string]string, 100)       //initialize with 100 keys
+
+	//get environmental info
 	ipport = os.Getenv("IPPORT")                 //get node's ipport from env
-	nodesPerCluster = os.Getenv("K")
+	var err error
+	nodesPerCluster, err = strconv.Atoi(os.Getenv("K"))
+	if err != nil {
+		log.Println(err)
+	}
 	view = strings.Split(os.Getenv("VIEW"), ",") //get node's initial view from env
 
+	//set vars
+	partitionID = 0
 	sortedView := ipsorting.SortIPs(view)
 	log.Println(sortedView)
 
